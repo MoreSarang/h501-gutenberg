d93f251d91cb5a5ad842e8caecfc5b9684abe86c
MoreSarang <sarangmore704@yahoo.com>
2025-09-23 04:14:58 -0400

    Update authors.py

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -5,11 +5,7 @@ def list_authors(by_languages=False, alias=False, df=None):
     """
     If alias=True and df is provided, returns list of author aliases
     sorted by translation count.
-
-    Supports placeholder for by_languages flag.
     """
     if alias and df is not None:
         return get_author_aliases(df)
-    
-    # You can add support for by_languages or other cases as needed
     return []
